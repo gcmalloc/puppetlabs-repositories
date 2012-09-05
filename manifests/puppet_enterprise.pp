e5c336cbc3415b1ca4fb16dc05689daaf2e30330
Will Hopper <whopper@cat.pdx.edu>
2012-09-05 14:44:20 -0700

    Make Fedora an unsupported platform for Enterprise repositories

--- manifests/puppet_enterprise.pp
+++ manifests/puppet_enterprise.pp
@@ -3,25 +3,33 @@ class repositories::puppet_enterprise {
   $pe_version = '2.5'
   case $osfamily {
     /(RedHat|Suse)/: {
-      $dist = $operatingsystem ? {
-        'SLES'  => 'sles',
-        default   => 'el',
-      }
+    case $operatingsystem {
+      /(CentOS|SLES)/: {
+          $dist = $operatingsystem ? {
+            'SLES'  => 'sles',
+            default   => 'el',
+          }
 
-      yumrepo { 'pe-main':
-          baseurl   => "http://neptune.puppetlabs.lan/${pe_version}/repos/${dist}-${os_maj_version}-${architecture}",
-          enabled   => '1',
-          gpgcheck  => '0',
-          descr     => "Puppet Labs Products ${os_version} - ${::architecture}",
-      }
+          yumrepo { 'pe-main':
+              baseurl   => "http://neptune.puppetlabs.lan/${pe_version}/repos/${dist}-${os_maj_version}-${architecture}",
+              enabled   => '1',
+              gpgcheck  => '0',
+              descr     => "Puppet Labs Products ${os_version} - ${::architecture}",
+          }
 
-      yumrepo { 'pe-SRPMS':
-          baseurl   => "http://neptune.puppetlabs.lan/${pe_version}/repos/${dist}-${os_maj_version}-srpms",
-          enabled   => '1',
-          gpgcheck  => '0',
-          descr     => "Puppet Labs Products ${os_version} - ${::architecture}",
+          yumrepo { 'pe-SRPMS':
+              baseurl   => "http://neptune.puppetlabs.lan/${pe_version}/repos/${dist}-${os_maj_version}-srpms",
+              enabled   => '1',
+              gpgcheck  => '0',
+              descr     => "Puppet Labs Products ${os_version} - ${::architecture}",
+          }
+        }
+        default: {
+          notice('Error: $operatingsystem is not a supported platform')
+        }
       }
     }
+
 
     'Debian': {
       apt::key { "puppetlabs":
